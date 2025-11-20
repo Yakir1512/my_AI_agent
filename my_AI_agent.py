0984a493f7ff8876356a544239d4f748b3cf80c6
BobmoKlat <yakir123hehr@gmail.com>
2025-11-20 16:47:38 +0200

    change no1

--- my_AI_agent.py
+++ my_AI_agent.py
@@ -1,19 +1,46 @@
-import os
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from openai import OpenAI
 from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv() 
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# client = OpenAI(api_key="sk-xxxxxxxxxxxxxxxxxxxx")
 
-def get_joke():
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "user", "content": "ספר לי בדיחה קצרה ומצחיקה"}
-        ]
-    )
-    return response.choices[0].message["content"]
+class first_App(App):
+    def build(self):
+        self.layout = BoxLayout(orientation="vertical", padding=20, spacing=10)
+
+        self.input = TextInput(hint_text="Enter a joke topic")
+        self.output = Label(text="Your joke will appear here")
+
+        btn = Button(text="Get Joke")
+        btn.bind(on_press=self.get_joke)
+
+        self.layout.add_widget(self.input)
+        self.layout.add_widget(btn)
+        self.layout.add_widget(self.output)
+        return self.layout
+
+
+    def get_joke(self, instance):
+        topic = self.input.text
+        print("what kind of a joke u want")
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role":"system" , "content":"you only respons with jokes you created"},
+                {"role": "user", "content": f"tell me a joke about {topic}"}
+            ]
+        )
+        joke = response.choices[0].message.content
+        self.output.text = joke
 
 if __name__ == "__main__":
-    print(get_joke())
+            first_App().run()
+    
